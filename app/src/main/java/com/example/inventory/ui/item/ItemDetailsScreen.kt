1ec0cecdc8a150134a74b84d06b92b2b21c894cc
Родион Дубинин <wryotu@gmail.com>
2026-05-22 21:49:08 +0300

    шаг4: реализована функция продажи товара с уменьшением количества

--- app/src/main/java/com/example/inventory/ui/item/ItemDetailsScreen.kt
+++ app/src/main/java/com/example/inventory/ui/item/ItemDetailsScreen.kt
@@ -104,7 +104,7 @@ fun ItemDetailsScreen(
     ) { innerPadding ->
         ItemDetailsBody(
             itemDetailsUiState = uiState.value,
-            onSellItem = { },
+            onSellItem = { viewModel.reduceQuantityByOne() },
             onDelete = { },
             modifier = Modifier
                 .padding(
@@ -138,7 +138,7 @@ private fun ItemDetailsBody(
             onClick = onSellItem,
             modifier = Modifier.fillMaxWidth(),
             shape = MaterialTheme.shapes.small,
-            enabled = true
+            enabled = !itemDetailsUiState.outOfStock
         ) {
             Text(stringResource(R.string.sell))
         }
