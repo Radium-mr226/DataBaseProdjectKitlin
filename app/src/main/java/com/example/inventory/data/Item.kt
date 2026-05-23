3a8703f915e298e422a1c651a62fe17d424b5d4e
2078-23 <cuddores@mail.ru>
2026-05-23 12:23:19 +0300

    добавлен недостаяющий интерфейс ItemDao

--- app/src/main/java/com/example/inventory/data/Item.kt
+++ app/src/main/java/com/example/inventory/data/Item.kt
@@ -40,20 +40,3 @@ data class Item(
     val quantity: Int
 )
 
-@Dao
-interface ItemDao {
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insert(item: Item)
-
-    @Update
-    suspend fun update(item: Item)
-
-    @Delete
-    suspend fun delete(item: Item)
-
-    @Query("SELECT * from items WHERE id = :id")
-    fun getItem(id: Int): Flow<Item>
-
-    @Query("SELECT * from items ORDER BY name ASC")
-    fun getAllItems(): Flow<List<Item>>
-}
